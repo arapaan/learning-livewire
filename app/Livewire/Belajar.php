53c04a7910a1adc3f494425db41c134488cad6bf
arapaan <arfan.arrasyid.78@gmail.com>
2025-02-13 16:13:05 +0700

    first

---
+++ app/Livewire/Belajar.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Belajar extends Component
+{
+    // public $nama = 'Arapaan';
+    public function render()
+    {
+        return view('livewire.belajar');
+    }
+}
---
+++ resources/views/livewire/belajar.blade.php
@@ -0,0 +1,24 @@
+<div>
+    <div class="mb-3">
+        {{-- <input type="text" class="form-control" wire:model="nama"> --}}
+        {{-- <textarea class="form-control" wire:model="nama"></textarea> --}}
+        {{-- <input type="radio" name="jenis_kelamin" value="Laki" wire:model="nama"> Laki
+        <input type="radio" name="jenis_kelamin" value="Perempuan" wire:model="nama"> perempuan --}}
+
+        {{-- <select class="form-select" wire:model="nama">
+            <option hidden>Pilih Jenis Kelamin</option>
+            <option value="Laki-Laki">Laki-Laki</option>
+            <option value="Perempuan">Perempuan</option>
+        </select> --}}
+        {{-- <input
+            @if(@show_password == 'show')
+            type="text"
+            @else
+            type="password"
+            @endif
+            class="form-control" wire:model="show_password" value="show"
+        >
+        <label for="">Show Password</label>
+        <input type="checkbox" wire:model="show_password" value="show"> --}}
+    </div>
+</div>
